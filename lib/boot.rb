0fc5212662d5e27f258c636b2e31111f958532d9
bbarr <brendanbarr@songlinedesign.com>
2011-04-06 21:28:10 -0400

    new mongo urls

--- lib/boot.rb
+++ lib/boot.rb
@@ -15,4 +15,10 @@ Dir.glob(File.join(File.dirname(__FILE__), "./apps/*.rb")) { |file| require file
 Dir.glob(File.join(File.dirname(__FILE__), "./models/*.rb")) { |file| require file }
 
 # setup mote with mongoDB connection
-Mote.db = Mongo::Connection.new.db("brendanbarr", :pk => Mote::PkFactory)
+if env['MONGOHQ_URL']
+  uri = URI.parse(ENV['MONGOHQ_URL'])
+  conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+  Mote.db = conn.db(uri.path.gsub(/^\//, ''), :pk => Mote::PkFactory)
+else
+  Mote.db = Mongo::Connection.new.db("brendanbarr", :pk => Mote::PkFactory)
+end
