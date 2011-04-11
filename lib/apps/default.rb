7b3955153a576e50e54f86c535ba248cf5d0f428
Brendan Barr <bbarr1384@gmail.com>
2011-04-11 15:57:06 -0400

    new design, basic layouts

--- lib/apps/default.rb
+++ lib/apps/default.rb
@@ -1,10 +1,28 @@
 module BB
   class Default < Base
    
-   get "/" do
-     @posts = Post.find
-     haml :"default/index"
-   end
+    get "/" do
+      @posts = Post.find.to_a
+      @categories = @posts.map { |p| p.category }.uniq unless @posts.size == 0
+      haml :"default/index"
+    end
     
+    get "/:category" do
+      @posts = Post.find(:category => params[:category]).to_a
+      
+      haml (@posts.size > 0 ? "default/index" : "default/404").to_sym
+        
+    end
+    
+    get "/:category/:post" do
+      @post = Post.find_one :uri_title => params[:post]
+ 
+      if @post
+        haml :"default/post"
+      else
+        haml :"default/404"
+      end
+    end
+
   end
 end
