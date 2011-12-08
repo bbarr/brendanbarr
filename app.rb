34f1b5ff76962be4660a3e15d7cc29a36910d3d8
bbarr <brendanbarr.web@gmail.com>
2011-12-08 00:10:09 -0500

    a

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ module BB
     
     #config
     set :static, true
-    set :public, File.join(File.dirname(__FILE__), './public/')
+    set :public_folder, File.join(File.dirname(__FILE__), './public/')
     
     #routes
     get "/" do
