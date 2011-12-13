6ad9d57590b51dab2da245fc9ae604ff8a49f08d
bbarr <brendanbarr.web@gmail.com>
2011-12-12 23:20:58 -0500

    subdomains test

--- app.rb
+++ app.rb
@@ -15,16 +15,25 @@ module BB
     end
     
     get "/" do
+      unless request.subdomains.empty?
+        redirect '/' + request.subdomains.first
+      end
       haml :index
     end
+  end
+end
 
-    get "/json-enforcer" do
-      haml :json_enforcer
+# We re-open the request class to add the subdomains method
+module Rack
+  class Request
+    def subdomains(tld_len=1) # we set tld_len to 1, use 2 for co.uk or similar
+      # cache the result so we only compute it once.
+      @env['rack.env.subdomains'] ||= lambda {
+        # check if the current host is an IP address, if so return an empty array
+        return [] if (host.nil? ||
+                      /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.match(host))
+        host.split('.')[0...(1 - tld_len - 2)] # pull everything except the TLD
+      }.call
     end
-
-    get "/marker" do
-      haml :marker
-    end
-
   end
 end
