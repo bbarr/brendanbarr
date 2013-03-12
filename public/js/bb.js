dc1343c83e557f29448c18aba080f81e23898124
bbarr <bbarr1384@gmail.com>
2013-03-12 14:23:13 -0400

    adding work section

--- public/js/bb.js
+++ public/js/bb.js
@@ -6,7 +6,7 @@
       $window = $('html,body'),
       $project_link = $('#project_link'),
       $bio_link = $('#bio_link'),
-      $contact_link = $('#work_link'),
+      $work_link = $('#work_link'),
       $internals = $project_link.add($contact_link).add($bio_link).add($work_link),
       $bio = $('#bio'),
       app, demo;
